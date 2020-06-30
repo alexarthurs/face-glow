2e7915bbcaeb03256867d8957cb3eb4f3e01e5d1
Alex Arthurs <info@alexarthurs.com>
2020-06-29 23:43:03 -0400

    Added Electron Storage

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import KeyLight from '@/components/KeyLight'
 
 Vue.use(Router)
+Vue.mixin({
+	data: function() {
+		return {
+			store: new Store()
+		};
+	}
+});
+
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'KeyLight',
+      component: KeyLight
     }
   ]
 })
